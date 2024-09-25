1fe4f4fd52725a2f30a8761869deacb9f1ccceae
javierli0829 <a2982601@gmail.com>
2024-09-25 23:36:43 +0800

    initial mat-table

--- whatmeowmeow/src/app/home/home.component.ts
+++ whatmeowmeow/src/app/home/home.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+export interface PeriodicElement {
+  type: string;
+  cost: number;
+  description: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {type: 'Food', cost: 1000, description: 'some description'},
+  {type: 'Clothing', cost: 24500, description: 'some description'}
+];
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: `
-  <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  `,
+  imports: [MatTableModule],
+  templateUrl: './home.component.html' ,
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  displayedColumns: string[] = ['type', 'cost', 'description'];
+  dataSource = ELEMENT_DATA;
 }
