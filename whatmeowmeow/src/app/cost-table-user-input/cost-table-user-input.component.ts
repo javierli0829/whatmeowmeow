a01eec487ba330007c04705a725ccd7b4755c7c8
javierli0829 <a2982601@gmail.com>
2024-10-03 01:43:56 +0800

    build a popup and add item to the list

---
+++ whatmeowmeow/src/app/cost-table-user-input/cost-table-user-input.component.ts
@@ -0,0 +1,54 @@
+import { Component, inject, model, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
+@Component({
+  selector: 'cost-table-user-input',
+  templateUrl: 'cost-table-user-input.component.html',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+})
+
+export class CostTableUserInputDialog {
+  readonly dialogRef = inject(MatDialogRef<CostTableUserInputDialog>);
+
+  typesCategory: string[] = ["Food", "Housing", "Clothing", "Transportation"];
+
+  dateInput: Date = new Date();
+  typeInput: string = "";
+  amountInput: number = 0;
+  descriptionInput: string = "";
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  onOkayClick(): void{
+    console.log("onOkay")
+    this.dialogRef.close({date: this.dateInput, type: this.typeInput, cost: this.amountInput, description: this.descriptionInput})
+  }
+}
--- whatmeowmeow/src/app/cost-table/cost-table.component.ts
+++ whatmeowmeow/src/app/cost-table/cost-table.component.ts
@@ -1,30 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDialog } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import {CostTableUserInputDialog} from '../cost-table-user-input/cost-table-user-input.component';
 
 interface CostTableElement {
+  date: Date;
   type: string;
   cost: number;
   description: string;
-  date: Date;
 }
-
-var ELEMENT_DATA: CostTableElement[] = [
-  {type: 'Food', cost: 1000, description: 'some description', date: new Date()},
-  {type: 'Clothing', cost: 24500, description: 'some description', date: new Date()}
-];
 
 @Component({
   selector: 'app-cost-table',
   standalone: true,
-  providers: [provideNativeDateAdapter()],
-  imports: [MatTableModule, MatButtonModule, MatInputModule, MatDatepickerModule, MatFormFieldModule, MatSelectModule, FormsModule],
+  imports: [
+    MatTableModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule
+  ],
   templateUrl: './cost-table.component.html',
   styleUrl: './cost-table.component.css'
 })
@@ -33,32 +36,22 @@ export class CostTableComponent {
   // Constructor
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<CostTableElement>();
-    ELEMENT_DATA.forEach(data => {
-      this.dataSource.data.push(data);
-    })
-    this.dateInput = new Date();
-    this.typeInput = "";
-    this.amountInput = 0;
-    this.descriptionInput = "";
   }
 
-  appendList(date: Date, type: string, cost: number, description: string): void{
-    this.dataSource.data.push({type, cost, description, date});
-    this.dataSource.data = this.dataSource.data;
-    // Refresh UI input
-    this.dateInput = new Date();
-    this.typeInput = "";
-    this.amountInput = 0;
-    this.descriptionInput = "";
-    console.log(this.dataSource.data);
+  readonly dialog = inject(MatDialog);
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CostTableUserInputDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result != undefined){
+        this.dataSource.data.push(result);
+        this.dataSource.data = this.dataSource.data;
+      }
+    });
   }
 
-  // Variable declaration
-  dateInput: Date = new Date();
-  typeInput: string = "";
-  amountInput: number = 0;
-  descriptionInput: string = "";
   dataSource = new MatTableDataSource<CostTableElement>();
   displayedColumns: string[] = ["type", "cost", "description"];
-  typesCategory: string[] = ["Food", "Housing", "Clothing", "Transportation"];
 }
+
